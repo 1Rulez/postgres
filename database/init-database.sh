9ac245ff28633a518cc11b45cd9324250762f849
Valentin Markin <markinva@cash-u.com>
2025-01-18 18:30:29 +0300

    TZ INFO DEFAULT

--- database/init-database.sh
+++ database/init-database.sh
@@ -8,4 +8,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE SCHEMA IF NOT EXISTS igs_kis;
     CREATE SCHEMA IF NOT EXISTS igs_casco;
     CREATE SCHEMA IF NOT EXISTS integration_sravni_kasco;
+    ALTER DATABASE "$POSTGRES_DB" SET TIME ZONE 'Europe/Moscow';
 EOSQL
